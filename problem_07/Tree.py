2b3d32240d016c091be13f6662617c0f82a8af11
flobaudry <33914132+flobaudry@users.noreply.github.com>
2022-12-07 22:31:11 +0100

    Challenge 7

--- problem_07/Tree.py
+++ problem_07/Tree.py
@@ -2,6 +2,7 @@ class Dir:
     def __init__(self, name, parent=None):
         self.name = name
         self.parent = parent
+        self.size = -1
         self.sub_dir = []
         self.leaves = []
 
@@ -27,13 +28,42 @@ class Dir:
         return self.parent
 
     def str_dir(self, depth=0):
-        offset =  depth*2*' '
-        res = f'{offset}- {self.name} (dir)\n'
+        offset = depth*2*' '
+        res = f'{offset}- {self.name} (dir, size={self.size if self.size != -1 else "Unknown"})\n'
         for leaf in self.leaves:
             res += f'{offset}  - {leaf.get_name()} (file, size={leaf.get_size()})\n'
         for sub_dir in self.sub_dir:
             res += sub_dir.str_dir(depth+1)
         return res
+
+    def compute_size(self):
+        self.size = 0
+        for leaf in self.leaves:
+            self.size += leaf.get_size()
+        for sub_dir in self.sub_dir:
+            self.size += sub_dir.compute_size()
+        return self.size
+
+    def less_than(self, amount):
+        size = self.size if self.size <= amount else 0
+        self.compute_size()
+        for sub_dir in self.sub_dir:
+            size += sub_dir.less_than(amount)
+        return size
+
+    def get_size(self):
+        return self.size
+
+    def find_closest(self, target):
+        if self.size < target:
+            return None
+        else:
+            choices = [self.size]
+            for sub_dir in self.sub_dir:
+                closest = sub_dir.find_closest(target)
+                if closest is not None:
+                    choices.append(closest)
+            return min(choices)
 
 
 class Leaf:
--- problem_07/problem_07.py
+++ problem_07/problem_07.py
@@ -18,18 +18,26 @@ def get_tree(f):
             if inputs[0] == 'dir':
                 working_directory.add_sub_dir(Dir(inputs[1], working_directory))
             else:
-                working_directory.add_leaf(Leaf(inputs[1], inputs[0]))
+                working_directory.add_leaf(Leaf(inputs[1], int(inputs[0])))
     return file_system
-
-
 
 
 def solve1(f):
     file_system = get_tree(f)
-    print(file_system.str_dir())
+    file_system.compute_size()
+    return file_system.less_than(100000)
+
+
+def find_closest(file_system, min_size):
+    min_dir_size = 30000000
+
 
 def solve2(f):
-    pass
+    file_system = get_tree(f)
+    file_system.compute_size()
+    target = 30000000 - (70000000 - file_system.get_size())
+    return file_system.find_closest(target)
+
 
 def main():
     with open('inputs.txt', 'r') as f:
