10232440e4c8bba64de3ae9cb865d9e5bf73be67
flobaudry <33914132+flobaudry@users.noreply.github.com>
2022-12-07 22:53:48 +0100

    All files generated

---
+++ problem_24/problem_24.py
@@ -0,0 +1,17 @@
+def solve1(inputs):
+    pass
+
+
+def solve2(inputs):
+    pass
+
+
+def main():
+    with open('inputs.txt', 'r') as f:
+        print(solve1(f))
+        f.seek(0)
+        print(solve2(f))
+
+
+if __name__ == '__main__':
+    main()
